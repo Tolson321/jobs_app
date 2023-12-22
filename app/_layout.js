0e592e558dda1891d0dd9d97154e851f43ca5d4c
Tom Olson <57242391+Tolson321@users.noreply.github.com>
2023-12-22 13:47:56 -0500

    more font stuff

--- app/_layout.js
+++ app/_layout.js
@@ -8,8 +8,16 @@ SplashScreen.preventAutoHideAsync();
 const Layout = () => {
     const [fontsLoaded] = useFonts({
         DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMBRegular: require('../assets/fonts/DMSans-Regular.ttf'),
     })
-    return <Stack />;
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded])
+    if (!fontsLoaded) return null;
+    return <Stack onLayout={onLayoutRootView} />;
 }
 
 export default Layout;
